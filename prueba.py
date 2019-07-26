904696a201b43f931e89d653814996f5b59fab32
sstuff <nandovm95@gmail.com>
2019-07-26 13:52:10 +0200

    añadido archivo de prueba

---
+++ prueba.py
@@ -0,0 +1,17 @@
+ 
+from __future__ import division
+from Processor import Processor
+from multithread.Capturer import Capturer
+from collections import deque
+import imutils
+import random as rng
+import numpy as np
+import argparse
+import cv2
+import chess
+
+def main():
+	video_capturer = Capturer 
+	
+
+main()
