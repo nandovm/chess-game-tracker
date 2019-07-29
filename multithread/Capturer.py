7b1c9676146582cc8a11466fef1fe6747a5333e8
sstuff <nandovm95@gmail.com>
2019-07-29 19:46:05 +0200

    refined capture algorithm to get relevant images

--- multithread/Capturer.py
+++ multithread/Capturer.py
@@ -1,4 +1,6 @@
 from threading import Thread
+from skimage.measure import compare_ssim
+import imutils
 import cv2
 
 class Capturer:
@@ -8,9 +10,15 @@ class Capturer:
     """
 
     def __init__(self, src=0):
-        self.stream = cv2.VideoCapture(src)
+        self.old_score = 0.02
+        self.switch = True
+        self.stream = cv2.VideoCapture(src)      
+        self.thres = 0.09                    
         (self.grabbed, self.image_ini) = self.stream.read()
-        self.image_next = image_ini
+        print("lectura")
+        self.image_ini = imutils.resize(self.image_ini, width=400)
+        self.image_next = self.image_ini
+        self.image_list = [self.image_ini]
         self.stopped = False
         self.rise = False
         self.doublerise=False
@@ -21,32 +29,51 @@ class Capturer:
 
     def get(self):
         while not self.stopped:
-            if not self.grabbed or get_ssmi(imageA = image_ini, imageB = image_next):
+            if not self.grabbed:
+                
                 self.stop()
             else:
                 (self.grabbed, self.image_next) = self.stream.read()
+                if self.grabbed: #para la ultima lectura
+                    self.image_next = imutils.resize(self.image_next, width=400)
+                    new_score = self.get_ssmi(imageA = self.image_ini, imageB = self.image_next)
+                    print(new_score*100)
+                    #print(self.old_score*100)
+                    if self.switch: #subida
+                        if abs(new_score*100 - self.old_score*100) > 2.5:
+                        #self.switch = not self.switch
+                        #if self.switch:
+                            print("SWITCH")
+                            self.switch = not self.switch
+                    else:
+                        #print("AKI")
+                        if abs(new_score*100 - self.old_score*100) < 0.5:
+                            self.image_list.append(self.image_next)
+                            
+                            self.image_ini = self.image_next#TODO calcular histograma 
+                            (self.grabbed, self.image_next) = self.stream.read()
+                            self.image_next = imutils.resize(self.image_next, width=400)
 
+                            self.old_score = self.get_ssmi(imageA = self.image_ini, imageB = self.image_next)
+                            self.switch = not self.switch
+                            print("ANYADE" + str(self.old_score*100))
+                            
+                #cv2.imshow("", self.image_next)
+                #cv2.waitKey(0)
+                #cv2.destroyAllWindows()
     def stop(self):
-        if not self.rise:
-            self.rise = True
-            image_ini = image_next
-        elif not self.doublerise:
             self.stopped = True
 
     def get_ssmi(self, imageA, imageB):
-        
-        imageA = imutils.resize(imageA, height=400)
-        imageB = imutils.resize(imageB, height=400)
 
         # convert the images to grayscale
-        grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
-        grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
-        
+        #grayA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+        #grayB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+        hist1 = cv2.calcHist([imageA],[0],None,[256],[0,256])
+        hist2 = cv2.calcHist([imageB],[0],None,[256],[0,256])
         # compute the Structural Similarity Index (SSIM) between the two
         # images, ensuring that the difference image is returned
-        (score, diff) = compare_ssim(grayA, grayB, full=True)
-        diff = (diff * 255).astype("uint8")
-        if(diff > 0.92):
-            return False
-        else:
-            return True
+        #(score, diff) = compare_ssim(grayA, grayB, full=True)
+        #iff = (diff * 255).astype("uint8")
+        score = cv2.compareHist(hist1,hist2,cv2.HISTCMP_BHATTACHARYYA)
+        return score
